4a1d12590ad439639c1d72c566da40b4f595d6be
cumulus13 <cumulus13@gmail.com>
2023-05-02 10:43:20 +0700

    version: 0.31 ~ 2023-05-02 10:43:17:163294

--- django.py
+++ django.py
@@ -12,16 +12,26 @@ import argparse
 import shutil
 import re
 from pydebugger.debug import debug
+import tenacity
+import jsoncolor
+import traceback
 
 print("PID:", os.getpid())
 
 class Fanout(object):
 
-    CONFIGNAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cust_fanout.ini')
+    CONFIGNAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'django.ini')
     CONFIG = configset(CONFIGNAME)
     CURRENT_HEIGHT = 0
     TERM_SIZE = shutil.get_terminal_size()
     AUTO_CLEAR = False
+
+    # define retry logic using tenacity
+    reconnect = tenacity.retry(
+        stop=tenacity.stop_after_attempt(10),  # maximum number of retries
+        wait=tenacity.wait_fixed(2),  # wait for 2 seconds between retries
+        retry=tenacity.retry_if_exception_type(pika.exceptions.AMQPConnectionError),  # retry only when AMQPConnectionError occurs
+    )
 
     @classmethod
     def set_color(self, level):
@@ -48,10 +58,13 @@ class Fanout(object):
     @classmethod
     def call_back(self, ch, met, prop, body):
         #print("%s [x] Received message: %s" % (datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M:%S.%f'), body))
-        debug(body = body, debug = 1)
+        debug(body = body)
         data = ''
         try:
             data = json.loads(body)
+            #debug(body = body, debug = 1)
+            jsoncolor.jprint(data)
+            print("-" * self.TERM_SIZE.columns)
             if self.AUTO_CLEAR:
                 if shutil.get_terminal_size().lines < self.CURRENT_HEIGHT:
                     if sys.platform == 'win32':
@@ -62,17 +75,18 @@ class Fanout(object):
                     self.CURRENT_HEIGHT = 0
                 else:
                     self.CURRENT_HEIGHT += 1
-            #print(
-                #make_colors(str(self.CURRENT_HEIGHT).zfill(2), 'lw', 'bl') + " " + \
+            print(
+                make_colors(str(self.CURRENT_HEIGHT).zfill(2), 'lw', 'bl') + " " + \
                 #make_colors(datetime.strftime(datetime.fromtimestamp(data.get('created')), '%Y/%m/%d %H:%M:%S:%f'), 'lc') + " [" + \
-                #make_colors(*self.set_color(data.get('levelname'))) + "][" + \
-                #make_colors(str(data.get('levelno')), 'lw', 'bl') + "] " + \
-                #make_colors(data.get('env'), 'lm') + " " + \
-                #make_colors(data.get('source'), 'ly') + " " + \
-                #make_colors(data.get('host'), 'lg') + " " + \
-                #make_colors(data.get('name'), 'b', 'y') + " " + \
-                #make_colors(data.get('message'), 'lc')
-            #)
+                make_colors(data.get('timestamp'), 'lc') + " [" + \
+                make_colors(*self.set_color(data.get('levelname'))) + "][" + \
+                make_colors(str(data.get('levelno')), 'lw', 'bl') + "] " + \
+                make_colors(data.get('env'), 'lm') + " " + \
+                make_colors(data.get('filename'), 'ly') + " " + \
+                make_colors(data.get('host'), 'lg') + " " + \
+                make_colors(data.get('name'), 'b', 'y') + " " + \
+                make_colors(data.get('message'), 'lc')
+            )
         except:
             try:
                 data = body.decode('utf-8')
@@ -89,11 +103,12 @@ class Fanout(object):
         ch.basic_ack(delivery_tag = met.delivery_tag)
 
     @classmethod
-    def connection(self, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13'):
+    @reconnect
+    def connection(self, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest'):
         #establish connection to RabbitMQ
-        #credentials = pika.PlainCredentials('root', 'Xxxnuxer')
-        # parameters = pika.ConnectionParameters(host='192.168.0.9', port = 5672, credentials=credentials)
-        #parameters = pika.URLParameters('amqp://root:Xxxnuxer13@192.168.0.9:5672/%2F')
+        #credentials = pika.PlainCredentials('guest', 'Xxxnuxer')
+        # parameters = pika.ConnectionParameters(host='127.0.0.1', port = 5672, credentials=credentials)
+        #parameters = pika.URLParameters('amqp://guest:guest@127.0.0.1:5672/%2F')
         parameters = pika.URLParameters('amqp://{}:{}@{}:{}/%2F'.format(username, password, hostname, port))
 
         conn = pika.BlockingConnection(parameters)
@@ -101,7 +116,12 @@ class Fanout(object):
 
         #create a queue to send messages
         #channel.queue_declare(queue='syslog', durable=True)
-        channel.exchange_declare(exchange = exchange_name, exchange_type='fanout', durable=True, auto_delete=False)
+        while 1:
+            try:
+                channel.exchange_declare(exchange = exchange_name, exchange_type='fanout', durable=True, auto_delete=False)
+                break
+            except Exception as e:
+                print(traceback.format_exc())
 
         result = channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
@@ -109,7 +129,7 @@ class Fanout(object):
         return channel, queue_name, conn
 
     @classmethod
-    def main(self, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13'):
+    def main(self, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest'):
         channel, queue_name,conn = self.connection(exchange_name, hostname, port, username, password)
         channel.basic_consume(queue = queue_name, on_message_callback = self.call_back, consumer_tag='all', auto_ack = False)
         #channel.basic_recover(requeue = True)
@@ -121,20 +141,21 @@ class Fanout(object):
         conn.close()
 
     @classmethod
-    def pub(self, message, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13'):
+    def pub(self, message, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest'):
         channel, _,conn = self.connection(exchange_name, hostname, port, username, password)
         channel.basic_publish(exchange_name, '', message)
-        conn.close
+        conn.close()
 
     @classmethod
     def usage(self):
         parser = argparse.ArgumentParser('fanout')
         parser.add_argument('EXCHANGE', default = 'django')
-        parser.add_argument('-H', '--host', help = 'Rabbitmmq Server Host/IP', default = '192.168.0.9')
+        parser.add_argument('-H', '--host', help = 'Rabbitmmq Server Host/IP, default: 127.0.0.1', default = '127.0.0.1')
         parser.add_argument('-P', '--port', help = 'Rabbitmmq Server Port, default: 5672', type = int, default = 5672)
-        parser.add_argument('-u', '--username', help = 'Rabbitmq admin/user name', default = 'root')
-        parser.add_argument('-p', '--password', help = 'Rabbitmq password admin/user', default = 'Xxxnuxer13')
+        parser.add_argument('-u', '--username', help = 'Rabbitmq admin/user name, default: guest', default = 'guest')
+        parser.add_argument('-p', '--password', help = 'Rabbitmq password admin/user, default: guest', default = 'guest')
         parser.add_argument('-a', '--auto-clear', help = 'Auto clear display if full', action = 'store_true')
+        parser.add_argument('-t', '--pub', help = 'Test pub message', action = 'store')
 
         cons = parser.add_subparsers()
 
@@ -148,8 +169,8 @@ class Fanout(object):
             args = parser.parse_args()
             if args.auto_clear:
                 self.AUTO_CLEAR = True
-            if hasattr(args, 'message'):
-                self.pub(args.message, args.EXCHANGE, args.host, args.port, args.username, args.password)
+            if args.pub:
+                self.pub(args.pub, args.EXCHANGE, args.host, args.port, args.username, args.password)
             else:
                 self.main(args.EXCHANGE, args.host, args.port, args.username, args.password)
 
