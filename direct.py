f7518708d0141e4d08dbbb7c1cccd60c13f22e4a
cumulus13 <cumulus13@gmail.com>
2023-05-16 09:48:21 +0700

    version: 0.33 ~ 2023-05-16 09:48:16:263443

--- direct.py
+++ direct.py
@@ -10,17 +10,18 @@ from configset import configset
 import json
 import argparse
 import shutil
+import jsoncolor
 from pydebugger.debug import debug
 
 print("PID:", os.getpid())
 
-class Direct(object):    
+class Direct(object):
 
     CONFIGNAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cust_direct.ini')
     CONFIG = configset(CONFIGNAME)
     CURRENT_HEIGHT = 0
     TERM_SIZE = shutil.get_terminal_size()
-    
+
     @classmethod
     def set_color(self, level):
         if 'emerg' in 'emerge' in str(level):
@@ -41,8 +42,8 @@ class Direct(object):
             return "DEBUG", self.CONFIG.get_config('LEVEL_DEBUG', 'fore', value='yellow'), self.CONFIG.get_config('LEVEL_DEBUG', 'back', value='black')
         else:
             return "UNKNOWN", self.CONFIG.get_config('LEVEL_UNKNOWN', 'fore', value='red'), self.CONFIG.get_config('LEVEL_UNKNOWN', 'back', value='white')
-    
-    
+
+
     @classmethod
     def call_back(self, ch, met, prop, body):
         #print("%s [x] Received message: %s" % (datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M:%S.%f'), body))
@@ -57,72 +58,82 @@ class Direct(object):
             self.CURRENT_HEIGHT = 0
         else:
             self.CURRENT_HEIGHT += 1
-        print(
-            make_colors(str(self.CURRENT_HEIGHT).zfill(2), 'lw', 'bl') + " " + \
-            make_colors(datetime.strftime(datetime.fromisoformat(data.get('timestamp')), '%Y/%m/%d %H:%M:%S:%f'), 'lc') + " [" + \
-            make_colors(*self.set_color(data.get('severity'))) + "] " + \
-            make_colors(data.get('facility'), 'lm') + " " + \
-            make_colors(data.get('ip'), 'ly') + " " + \
-            make_colors(data.get('hostname'), 'lg') + " " + \
-            make_colors(data.get('appname'), 'b', 'y') + " " + \
-            make_colors(data.get('message'), 'lc')
-        )
+        try:
+            print(
+                make_colors(str(self.CURRENT_HEIGHT).zfill(2), 'lw', 'bl') + " " + \
+                make_colors(datetime.strftime(datetime.fromisoformat(data.get('timestamp')), '%Y/%m/%d %H:%M:%S:%f'), 'lc') + " [" + \
+                make_colors(*self.set_color(data.get('severity'))) + "] " + \
+                make_colors(data.get('facility'), 'lm') + " " + \
+                make_colors(data.get('ip'), 'ly') + " " + \
+                make_colors(data.get('hostname'), 'lg') + " " + \
+                make_colors(data.get('appname'), 'b', 'y') + " " + \
+                make_colors(data.get('message'), 'lc')
+            )
+        except:
+            jsoncolor.jprint(data)
+
         ch.basic_ack(delivery_tag = met.delivery_tag)
-    
+
     @classmethod
-    def connection(self, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13', queue = ''):
+    def connection(self, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest', queue = ''):
         #establish connection to RabbitMQ
-        #credentials = pika.PlainCredentials('root', 'Xxxnuxer')
-        # parameters = pika.ConnectionParameters(host='192.168.0.9', port = 5672, credentials=credentials)
-        #parameters = pika.URLParameters('amqp://root:Xxxnuxer13@192.168.0.9:5672/%2F')
+        #credentials = pika.PlainCredentials('guest', 'Xxxnuxer')
+        # parameters = pika.ConnectionParameters(host='127.0.0.1', port = 5672, credentials=credentials)
+        #parameters = pika.URLParameters('amqp://root:Xxxnuxer13@127.0.0.1:5672/%2F')
         parameters = pika.URLParameters('amqp://{}:{}@{}:{}/%2F'.format(username, password, hostname, port))
-        
-        conn = pika.BlockingConnection(parameters)
+
+        while 1:
+            try:
+                conn = pika.BlockingConnection(parameters)
+                break
+            except:
+                pass
         channel = conn.channel()
-        
+
         #create a queue to send messages
         #channel.queue_declare(queue='syslog', durable=True)
         channel.exchange_declare(exchange = exchange_name, exchange_type='direct', durable=True, auto_delete=False)
-        
+
         result = channel.queue_declare(queue=queue, exclusive=False, durable = True)
         queue_name = result.method.queue
+        debug(queue_name = queue_name, debug = 1)
         #channel.queue_bind(exchange = exchange_name, queue = queue_name)
         channel.queue_bind(exchange = exchange_name, queue = queue)
         return channel, queue_name, conn
-    
+
     @classmethod
-    def main(self, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13', queue = ''): 
-        channel, queue_name,conn = self.connection(exchange_name, hostname, port, username, password, queue)
+    def main(self, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest', queue = ''):
+        channel, queue_name, conn = self.connection(exchange_name, hostname, port, username, password, queue)
         channel.basic_consume(queue = queue_name, on_message_callback = self.call_back, consumer_tag='all', auto_ack = False)
         #channel.basic_recover(requeue = True)
         try:
             channel.start_consuming()
         except KeyboardInterrupt:
             print("exit ...")
-        
+
         conn.close()
-        
+
     @classmethod
-    def pub(self, message, exchange_name, hostname = '192.168.0.9', port = 5672, username = 'root', password = 'Xxxnuxer13', queue = ''): 
+    def pub(self, message, exchange_name, hostname = '127.0.0.1', port = 5672, username = 'guest', password = 'guest', queue = ''):
         channel, _,conn = self.connection(exchange_name, hostname, port, username, password, queue)
         channel.basic_publish(exchange_name, '', message)
         conn.close
-    
+
     @classmethod
     def usage(self):
         parser = argparse.ArgumentParser('direct')
         parser.add_argument('EXCHANGE')
-        parser.add_argument('-H', '--host', help = 'Rabbitmmq Server Host/IP', default = '192.168.0.9')
+        parser.add_argument('-H', '--host', help = 'Rabbitmmq Server Host/IP', default = '127.0.0.1')
         parser.add_argument('-P', '--port', help = 'Rabbitmmq Server Port, default: 5672', type = int, default = 5672)
-        parser.add_argument('-u', '--username', help = 'Rabbitmq admin/user name', default = 'root')
-        parser.add_argument('-p', '--password', help = 'Rabbitmq password admin/user', default = 'Xxxnuxer13')
+        parser.add_argument('-u', '--username', help = 'Rabbitmq admin/user name', default = 'guest')
+        parser.add_argument('-p', '--password', help = 'Rabbitmq password admin/user', default = 'guest')
         parser.add_argument('-q', '--queue', help = 'Rabbitmq Queue name, default: "logs"', default = 'logs')
-        
+
         cons = parser.add_subparsers()
-        
+
         parser_cons = cons.add_parser('push')
         parser_cons.add_argument('-m', '--message', help = 'send message')
-        
+
         if len(sys.argv) == 1:
             parser.print_help()
         else:
@@ -132,6 +143,6 @@ class Direct(object):
                 self.pub(args.message, args.EXCHANGE, args.host, args.port, args.username, args.password, args.queue)
             else:
                 self.main(args.EXCHANGE, args.host, args.port, args.username, args.password, args.queue)
-            
+
 if __name__ == '__main__':
     Direct.usage()
